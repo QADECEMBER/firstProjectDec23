c33b2b8a4bdfd3eb7a0b1ace4b8ae00b68dd1c88
Claire2912 <maclairy@gmail.com>
2024-05-18 12:50:37 -0500

    first commit

---
+++ src/test/java/variousConcepts/LearnLocators.java
@@ -0,0 +1,21 @@
+package variousConcepts;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class LearnLocators {
+
+	@Before
+	public void setUp() {
+		
+		
+		WebDriver driver = new ChromeDriver();
+	}
+	
+	@Test
+	public void testLocators() {
+		
+	}
+}
